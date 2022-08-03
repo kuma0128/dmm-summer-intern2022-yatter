866914b453a5e7af76e052dc4e663cc1793d09d0
taisei <taisei@taiseinoMacBook-Pro.local>
2022-08-03 14:42:54 +0900

    add statuses route

---
+++ app/handler/statuses/router.go
@@ -0,0 +1,25 @@
+package statuses
+
+import (
+	"net/http"
+
+	"yatter-backend-go/app/app"
+
+	"github.com/go-chi/chi"
+)
+
+// Implementation of handler
+type handler struct {
+	app *app.App
+}
+
+// Create Handler for `/v1/statuses/`
+func NewRouter(app *app.App) http.Handler {
+	r := chi.NewRouter()
+
+	h := &handler{app: app}
+	r.Post("/", h.Create)
+	r.Get("/{id}", h.Getstatus)
+
+	return r
+}
