b3bb4031b6418425361a0c9dfecfef3fe222150e
taisei <taisei@taiseinoMacBook-Pro.local>
2022-08-04 11:22:21 +0900

    fix status struct(Accout)

--- app/domain/object/status.go
+++ app/domain/object/status.go
@@ -9,7 +9,7 @@ type (
 		S_id StatusID `json:"-" db:"id"`
 
 		// The internal ID of the account
-		U_id AccountID `json:"-" db:"account_id"`
+		U_id Account `json:"-" db:"account_id"`
 
 		// content
 		Content string `json:"content,omitempty" db:"content"`
